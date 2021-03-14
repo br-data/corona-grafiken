020f3ff586c982df2a6f8f1e15516d53bc6ab886
Steffen Kühne <stekhn@gmail.com>
2021-03-14 15:38:45 +0100

    Move font loading to index.html

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap');
-
   html,
   body {
     margin: 0;
@@ -10,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    font-family: Helvetica, Arial, sans-serif;
+    font-family: "Open Sans", OpenSans, sans-serif;
     font-size: 16px;
     line-height: 1.6;
   }
