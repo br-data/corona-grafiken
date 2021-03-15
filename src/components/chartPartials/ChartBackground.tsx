1f7957226301ff6d62e1be354a6c644b27eb6789
Steffen Kühne <stekhn@gmail.com>
2021-03-15 19:23:01 +0100

    Make sure SVG gradients look the same when exported

--- src/components/chartPartials/ChartBackground.tsx
+++ src/components/chartPartials/ChartBackground.tsx
@@ -14,7 +14,13 @@ export const ChartBackground: React.FC<ChartBackgroundProps> = ({
   return (
     <>
       <defs>
-        <radialGradient id="radial-gradient">
+        <radialGradient
+          id="radial-gradient"
+          cx={width / 2}
+          cy={height / 2}
+          r={(width / 2 + height / 2) / 2}
+          gradientUnits="userSpaceOnUse"
+        >
           <stop offset=".25" stopColor={chartColors.backgroundPrimary}></stop>
           <stop offset="1" stopColor={chartColors.backgroundSecondary}></stop>
         </radialGradient>
